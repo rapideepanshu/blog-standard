4aa0b713710f6673adb36554271f7c2df58afc9b
rapideepanshu <deepanshu@rapidinnovation.dev>
2023-05-16 18:26:12 +0530

    Added validation and image create openAI api

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -4,10 +4,13 @@ import clientPromise from "../../lib/mongodb";
 import { ObjectId } from "mongodb";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {getAppProps} from "../../utils/getAppProps";
+import { getAppProps } from "../../utils/getAppProps";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Post(props) {
-  console.log("PROPS", props);
+  const [showDeleteConfirm, setShowDeleteCofirm] = useState(false);
+
   return (
     <div className="overflow-auto h-full">
       <div className="max-w-screen-sm mx-auto">
@@ -31,9 +34,20 @@ export default function Post(props) {
         <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
           Blog post
         </div>
+        <div className="my-1">
+          <Image src={props.image} alt="image" height={200} width={640} />
+        </div>
         <div
           dangerouslySetInnerHTML={{ __html: props.postContent || "" }}
         ></div>
+        <div className="my-4">
+          {!showDeleteConfirm && (
+        <button onClick={()=>setShowDeleteCofirm(true)} className="bg-red-600 text-white tracking-wider w-full text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-red-700 transition-colors block">
+        Delete Post
+      </button>
+          )}
+        
+        </div>
       </div>
     </div>
   );
@@ -46,8 +60,8 @@ Post.getLayout = function getLayout(page, pageProps) {
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
     const session = await getSession(ctx.req, ctx.res);
-    const props=  await getAppProps(ctx)
-    console.log('PROPSSSS:',props)
+    const props = await getAppProps(ctx);
+    // console.log('PROPSSSS:',props)
     const client = await clientPromise;
     const db = client.db("SmartBlogger");
     const post = await db.collection("posts").findOne({
@@ -70,7 +84,8 @@ export const getServerSideProps = withPageAuthRequired({
         title: post.title,
         metaDescription: post.metaDescription,
         keywords: post.keywords,
-        ...props
+        image: post.image ? post.image : "",
+        ...props,
       },
     };
   },
